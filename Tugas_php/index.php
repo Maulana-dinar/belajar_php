0ec7f353cd3f82266340d426cbd0534bd3389d88
Maulana-dinar <dinarmaulana2803@gmail.com>
2025-01-15 14:51:09 +0700

    nambah

---
+++ Tugas_php/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+          define ('GAJI_POKOK', 5000000 );
+          define ('BONUS', 500000);
+          define ('PAJAK', 0.12);
+ $jumlahproyek = 3;
+ $total = 0;
+ $gajibersih = 0;
+
+?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,14 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    <h3>
-       <?php
-           echo "hello world ini adalah halaman pertama PHP";
-           $var1 = 2000;
-           echo "jumlah var1 $var1"
-        ?>
-    </h3>
+<body>   
     
 </body>
 </html>
