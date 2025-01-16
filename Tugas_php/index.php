476e25e9d563d4994f8af8c89333758a45edf3a5
Maulana-dinar <dinarmaulana2803@gmail.com>
2025-01-16 10:24:31 +0700

    pembaruan

--- Tugas_php/index.php
+++ Tugas_php/index.php
@@ -6,14 +6,28 @@
     <title>Document</title>
 </head>
 <body>
-<?php
+    <?php
 
           define ('GAJI_POKOK', 5000000 );
           define ('BONUS', 500000);
           define ('PAJAK', 0.12);
- $jumlahproyek = 3;
- $total = 0;
- $gajibersih = 0;
+
+          
+             $jumlahproyek = 3;
+             $total_gaji;
+             $gajibersih;
+             
+
+        $jumlahproyek *=BONUS;
+        $total_gaji = GAJI_POKOK * PAJAK;
+        $pajak_asli = $total_gaji * PAJAK;
+        $gajibersih = $total_gaji - $pajak_asli;
+
+
+    echo    "ini bonus anda $jumlahproyek <br/>";
+    echo    "ini gaji anda setelah di tambah bonus $total_gaji <br/>";
+    echo    "pajak anda $pajak_asli <br/>";   
+    echo    "gaji bersih anda $gajibersih <br/>";
 
 ?>
 </body>
